7acecd1e92d395a9fd2f2d9060a946068cb36247
Nelson Rengifo <nrengifo3107@gmail.com>
2025-11-23 19:41:02 -0500

    Add comments to describe app.py functionality
    Added introductory comments explaining the purpose of the file.

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,16 @@
+# The main file of your backend.
+# When you run the project, this is the file you start.
 
+# What it does:
+
+# Creates the Flask app
+
+# Loads routes
+
+# Connects to the database
+
+# Starts the server
+
+# Defines global config/settings
+
+# Think of it as the "brainstem" that ties everything together.
